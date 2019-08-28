30633b47e0c19ec268a1a9feb1307a3c0a3709f9
Frederik Heymann <frederik.heymann@gmail.com>
2019-08-28 17:01:54 +0200

    added other functions for converting variables

--- as.onehot.R
+++ as.onehot.R
@@ -23,4 +23,46 @@ as_onehot <- function(data, var){
   return(data)
 }
 
+#' Converts to mean of zero and sd of one
+#' 
+#' takes a dataset and a variable and converts it into
+#' having a mean of zero, and a standard deviation of one.
+#' 
+#' @param data a data.frame
+#' @param var a variable in the data.frame 
+#' @export 
 
+
+as_standardized <- function(data, var){
+  
+  stopifnot(var %in% names(data))
+  stopifnot(is.numeric(data[[var]]))
+  
+  x <- data[[var]]
+  x <- x - mean(x)
+  x <- x / sd(x)
+  data[[var]] <- x
+  
+  return(data)
+}
+
+
+#' Converts data column to be ranging between one and zero
+#' 
+#' takes a dataset and a variable and converts it into
+#' numbers ranging between one and zero.
+#' 
+#' @param data a data.frame
+#' @param var a variable in the data.frame 
+#' @export 
+
+as_normalized <- function(data, var){
+  
+  stopifnot(var %in% names(data))
+  stopifnot(is.numeric(data[[var]]))  
+  
+  x <- data[[var]]
+  data[[var]] <- (x-min(x))/(max(x)-min(x))
+  
+  return(data)
+}
