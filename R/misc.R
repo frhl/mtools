a533480291b502d00c27a648853591f88a5a5403
Frederik Heymann <frederik.heymann@gmail.com>
2019-08-29 16:40:23 +0200

    added misc functions

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,87 @@
-#' easy perl-like regex
+##' @name Misc
+##' @rdname misc
+##'
+##' @title Helper functions
+##' 
+##' @description
+##' n converts vector or data.frame to numeric\\
+##' \%p\% paste the right and the left side together with no seperator\\
+##'
+##' @param x anything
+##' @param y anything
+##' @family Misc
+NULL
+
+# Convert vector or data.frame to numeric
+#' @name misc
+#' @rdname misc
+#' @export
+#' @examples 
+#' \dontrun{ ##input The object that should be converted to numeric.}
+n <- function(input) {
+  if(is.data.frame(input) | is.matrix(input)) {
+    return(data.frame(apply(input,2,function(z) as.numeric(as.character(z)))))
+  }else {
+    ## Simple version
+    return(as.numeric(as.character(input)))
+  }   
+}
+
+#' @name misc
+#' @rdname misc
+#' @export
+#'
+#' @examples
+#' "bla" %p% "alb"
+"%p%" <- function(x,y) {paste(x,y,sep="")}
+
+#' @name misc
+#' @rdname misc
+#' @export
+#'
+#' @examples
+#' "bla" %nin% c("alb","asg","faasg")
+'%nin%' <- function(x,y) {!(x %in% y)}
+
+#' @name misc
+#' @rdname misc
+#' @export
+#' @examples 
+#' \dontrun{c(1,2,3) %+=% 2}
+'%+=%' <- function(x,y) {x+y}
+
+#' @name misc
+#' @rdname misc
+#' @export
 #' 
-#' returns a perl like regex match
+'%-=%' <- function(x,y) {x-y}
+
+#' inverse of logit
+#' @name misc
+#' @rdname misc
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ##inverse of logit
+#' }
+invlogit <- function(x) exp(x)/(1 + exp(x))
+
+#'  logit
+#' @name misc
+#' @rdname misc
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ## logit
+#' }
+#' 
+logit= function(x)  log(x) - log(1 - x)
+
+#' easy perl-like regex
+#' @name misc
+#' @rdname misc
 #' 
 #' @param pattern the regex pattern
 #' @param text the text to be matched
@@ -13,8 +94,5 @@
 regex_find <- function(pattern, text, invert = F, perl = F){
   return(regmatches(text, regexpr(pattern, text, perl), invert))
 }
-
-
-
 
 
